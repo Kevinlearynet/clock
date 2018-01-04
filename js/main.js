e40bad5907c95137a5a23035d99aa9709b1953e4
Kevin Leary <info@kevinleary.net>
2018-01-04 12:06:39 -0500

    It begins.

---
+++ js/main.js
@@ -0,0 +1,96 @@
+/**
+ * iOS Clock App
+ */
+
+/**
+ * Javascript Clock
+ */
+function now() {
+	var time = new Date()
+	var hr = time.getHours()
+	var min = time.getMinutes()
+	var sec = time.getSeconds()
+
+	if ( hr > 12 ) {
+		hr -= 12
+	}
+	if ( hr < 10 ) {
+		hr = " " + hr
+	}
+	if ( min < 10 ) {
+		min = "0" + min
+	}
+	if ( sec < 10 ) {
+		sec = "0" + sec
+	}
+
+	return hr + ":" + min + ":" + sec
+}
+
+var startClock = function( next ) {
+	var time = now();
+	time = time.replace( /:/g, '<span class="colon">:</span>' );
+	$( '#clock' ).html( time );
+	var t = setTimeout( startClock, 500 );
+};
+
+function round( value, decimals ) {
+	return Number( Math.round( value + 'e' + decimals ) + 'e-' + decimals );
+}
+
+/**
+ * Weather Forecast
+ */
+function lookupWeather() {
+
+	// Current time for adjusting weather report
+	var time = new Date();
+	var hr = time.getHours();
+
+	// Find current geolocation
+	navigator.geolocation.getCurrentPosition( function( position ) {
+		var query = 'https://api.weather.gov/points/' + round( position.coords.latitude, 4 ) + ',' + round( position.coords.longitude, 4 ) + '/forecast';
+
+		var req = $.ajax( {
+			url: query,
+			dataType: 'json',
+			cache: false
+		} );
+
+		req.done( function( resp ) {
+			var today = new Date();
+			var offset = ( hr > 5 ) ? 0 : 1;
+			var weather = resp.properties.periods[ offset ];
+			weather.formattedDate = today.getMonth() + '/' + today.getDate();
+
+			$( '[data-weather]' ).each( function() {
+				var prop = $( this ).data( 'weather' );
+				var val = weather[ prop ];
+				$( this ).text( val );
+			} );
+
+			$( 'body' ).addClass( 'ready' );
+		} );
+
+		req.fail( function( err ) {
+			console.error( err );
+		} );
+	} );
+}
+
+// Disallow moving on iOS
+function blockMove( event ) {
+	event.preventDefault();
+}
+
+// Clock
+startClock();
+
+// Weather
+lookupWeather();
+
+// Re-check every 5 minutes
+setInterval( function() {
+	lookupWeather();
+	console.info( 'Re-checked weather after 5 mins...' )
+}, 300000 );
